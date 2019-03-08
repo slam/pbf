3a965b4fab53aef8380676158c939d996e028a87
Sahn Lam <slam@users.noreply.github.com>
2019-03-07 18:05:20 -0800

    Add console.log

--- index.js
+++ index.js
@@ -28,6 +28,7 @@ Pbf.prototype = {
     // === READING =================================================================
 
     readFields: function(readField, result, end) {
+        console.log(readField);
         end = end || this.length;
 
         while (this.pos < end) {
